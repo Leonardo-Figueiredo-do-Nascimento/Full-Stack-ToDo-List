fb1d81c7515cf6410fdc8d6e9e01b4bb7ca5ac83
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-08-14 15:33:01 -0300

    Animation for failling or concluding task

--- Front-end/src/components/Task Card/TaskCard.jsx
+++ Front-end/src/components/Task Card/TaskCard.jsx
@@ -2,8 +2,18 @@ import './TaskCard.css'
 
 function TaskCard(props) {
 
+  const statusStyle = () =>{
+    if(props.status === 'Completed'){
+      return 'completed'
+    } else if(props.status === 'Incomplete'){
+      return 'incomplete'
+    } else {
+      return null
+    };
+  }
+
   return (
-    <div className="task-div">
+    <div className="task-div" id={`${statusStyle()}`}>
       <p id='title'>{props.title}</p>
       <p id='status'>{props.status}</p>
       <p id='deadline'>{props.deadline}</p>
